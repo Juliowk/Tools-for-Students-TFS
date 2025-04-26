cf6f6401eaf9cc53806b30ca49fc4552b6ccb8bd
Júlio Elias De Sousa Rocha <julioeliasdesousarocha.wk@gmail.com>
2025-04-26 12:16:46 -0300

    feat: Adding the title and link of each tool

--- frontend/src/components/Index/ToolsComponent/desktop.tsx
+++ frontend/src/components/Index/ToolsComponent/desktop.tsx
@@ -1,6 +1,7 @@
 import { Card, Col, Row } from "react-bootstrap";
 import { BsChatLeftText, BsClockHistory } from "react-icons/bs";
 import { CiCalculator2 } from "react-icons/ci";
+import { FaRegLightbulb } from "react-icons/fa";
 import { GiEarthAmerica } from "react-icons/gi";
 
 const Desktop = () => {
@@ -31,8 +32,14 @@ const Desktop = () => {
         Mathematics <CiCalculator2 />
       </p>
       <Row xs={1} md={4} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col key={idx}>
+        <a
+          href="https://www.geogebra.org/calculator"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">GeoGebra</p>
+
+          <Col>
             <div className="ratio ratio-1x1">
               <Card className="bg-body-tertiary shadow">
                 <Card.Body className="d-flex flex-column justify-content-center align-items-center">
@@ -43,7 +50,67 @@ const Desktop = () => {
               </Card>
             </div>
           </Col>
-        ))}
+        </a>
+
+        <a
+          href="https://photomath.com/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Photomath</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                  <Card.Title className="fs-1">
+                    <CiCalculator2 size={50} />
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
+
+        <a
+          href="https://www.wolframalpha.com/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Wolfram Alpha</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
+                  <Card.Title className="fs-1">
+                    <CiCalculator2 size={50} />
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
+
+        <a
+          href="https://pt.khanacademy.org/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Khan Academy</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                  <Card.Title className="fs-1">
+                    <CiCalculator2 size={50} />{" "}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
       </Row>
 
       <hr className="w-50 mx-auto mt-5" />
@@ -52,11 +119,17 @@ const Desktop = () => {
         Languages <BsChatLeftText />
       </p>
       <Row xs={1} md={4} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col key={idx}>
+        <a
+          href="https://conjugacao.reverso.net/conjugacao-portugues.html"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Conjugador Reverso</p>
+
+          <Col>
             <div className="ratio ratio-1x1">
               <Card className="bg-body-tertiary shadow">
-                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
                   <Card.Title className="fs-1">
                     <BsChatLeftText size={50} />
                   </Card.Title>
@@ -64,7 +137,67 @@ const Desktop = () => {
               </Card>
             </div>
           </Col>
-        ))}
+        </a>
+
+        <a
+          href="https://pt.duolingo.com/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Duolingo</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
+                  <Card.Title className="fs-1">
+                    <BsChatLeftText size={50} />{" "}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
+
+        <a
+          href="https://www.spanishdict.com/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">SpanishDict</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
+                  <Card.Title className="fs-1">
+                    <BsChatLeftText size={50} />{" "}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
+
+        <a
+          href="https://pt.khanacademy.org/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Khan Academy</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                  <Card.Title className="fs-1">
+                    <BsChatLeftText size={50} />{" "}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
       </Row>
 
       <hr className="w-50 mx-auto mt-5" />
@@ -73,11 +206,17 @@ const Desktop = () => {
         Geography <GiEarthAmerica />
       </p>
       <Row xs={1} md={4} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col key={idx}>
+        <a
+          href="https://www.google.com.br/earth/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Google Earth</p>
+
+          <Col>
             <div className="ratio ratio-1x1">
               <Card className="bg-body-tertiary shadow">
-                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
                   <Card.Title className="fs-1">
                     <GiEarthAmerica size={50} />
                   </Card.Title>
@@ -85,7 +224,63 @@ const Desktop = () => {
               </Card>
             </div>
           </Col>
-        ))}
+        </a>
+
+        <a
+          href="https://www.seterra.com/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Seterra</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
+                  <Card.Title className="fs-1">
+                    <GiEarthAmerica size={50} />
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
+
+        <a className="link-dark link-underline link-underline-opacity-0">
+          <p className="fs-3 text-center mb-2">Ainda não declarado</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
+                  <Card.Title className="fs-1">
+                    <FaRegLightbulb size={50} />
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
+
+        <a
+          href="https://pt.khanacademy.org/"
+          target="_blank"
+          className="link-dark link-underline link-underline-opacity-0"
+        >
+          <p className="fs-3 text-center mb-2">Khan Academy</p>
+
+          <Col>
+            <div className="ratio ratio-1x1">
+              <Card className="bg-body-tertiary shadow">
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                  <Card.Title className="fs-1">
+                    <GiEarthAmerica size={50} />{" "}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </a>
       </Row>
     </>
   );
